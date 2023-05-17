d3e7e9134130463e6406b749c055fcb8323ccec9
Anna Gut <anna@yaak.ai>
2023-05-17 11:40:18 +0200

    feat: add validation and logging to Gato

--- cargpt/models/gato.py
+++ cargpt/models/gato.py
@@ -1,6 +1,9 @@
+from typing import Optional
+
 import more_itertools as mit
 import pytorch_lightning as pl
 import torch
+import wandb
 from einops import repeat, rearrange
 from hydra.utils import instantiate
 from jaxtyping import Float, Int
@@ -91,9 +94,7 @@ class Gato(pl.LightningModule, LoadableFromArtifact):
 
         self.predict_steps: int = self.hparams.predict_steps  # type: ignore[assignment]
 
-    def training_step(self, batch, batch_idx):
-        speed = self.unpack_batch_for_predictions(batch)
-
+    def _step(self, speed):
         speed_encoded = self.encodings.continues_values(speed)  # type: ignore[operator]
 
         # when more input data is added - concat vectors and pass as tensors encoded
@@ -102,8 +103,10 @@ class Gato(pl.LightningModule, LoadableFromArtifact):
             discrete_actions=speed_encoded[..., 1:],
         )
 
-        # compute loss
-        log_p = log_softmax(pred, dim=-1)
+        return pred, tgt_discrete_actions
+
+    def _compute_loss(self, predictions, tgt_discrete_actions):
+        log_p = log_softmax(predictions, dim=-1)
         # TODO: mask tokens when more than speed is available -> m(b,l) (2.3, eq 2)
         loss = -(
             torch.gather(
@@ -112,9 +115,62 @@ class Gato(pl.LightningModule, LoadableFromArtifact):
                 index=tgt_discrete_actions[:, :, None].to(torch.int64),
             )
         ).sum()
-
-        self.log("train_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
         return loss
+
+    def training_step(self, batch, batch_idx):
+        speed = self.unpack_batch_for_predictions(batch)
+        pred, tgt_discrete_actions = self._step(speed)
+        loss = self._compute_loss(pred, tgt_discrete_actions)
+        self.log(
+            "train/loss",
+            loss,
+            on_step=True,
+            on_epoch=True,
+            prog_bar=True,
+            sync_dist=True,
+            rank_zero_only=True,
+        )
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        speed = self.unpack_batch_for_predictions(batch)
+        pred, tgt_discrete_actions = self._step(speed)
+        loss = self._compute_loss(pred, tgt_discrete_actions)
+        self.log(
+            "val/loss",
+            loss,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+            sync_dist=True,
+            rank_zero_only=True,
+        )
+
+        if hasattr(self, "_tables"):
+            speed_predicted: Float[Tensor, "b t"] = self.invert_predictions(pred)
+            speed_gt: Float[Tensor, "b t"] = speed[..., -speed_predicted.shape[-1] :]
+            if table := self._tables.get("outputs"):
+                data: Float[Tensor, "b tx"] = rearrange(
+                    [  # type: ignore
+                        speed_predicted,
+                        speed_gt,
+                    ],
+                    "x b t -> b (t x)",
+                )
+
+                for row in data.tolist():
+                    table.add_data(*row)
+
+        return loss
+
+    def invert_predictions(
+        self, predictions: Float[Tensor, "b t e"]
+    ) -> Float[Tensor, "b t"]:
+        discrete: Int[Tensor, "b t"] = torch.argmax(predictions, dim=-1)
+        inverted = discrete.clone()
+        for encoding in reversed(self.encodings.continues_values):  # type: ignore[arg-type]
+            inverted = encoding.invert(inverted)
+        return inverted
 
     def forward(
         self,
@@ -173,7 +229,11 @@ class Gato(pl.LightningModule, LoadableFromArtifact):
             tgt_mask=tgt_mask,
         )
         pred = self.back_to_discrete(pred)
-        return pred, discrete_actions[:, split_point:]
+
+        # Reverse shift from split point
+        pred = pred[:, 1:, :]
+        discrete_actions = discrete_actions[:, (split_point + 1) :]
+        return pred, discrete_actions
 
     def unpack_batch_for_predictions(self, batch) -> Float[Tensor, "b t"]:
         clips = mit.one(batch["clips"].values())
@@ -190,3 +250,40 @@ class Gato(pl.LightningModule, LoadableFromArtifact):
             result["lr_scheduler"] = {**lr_scheduler_cfg, **{"scheduler": scheduler}}
 
         return result
+
+    def on_validation_epoch_start(self):
+        if (
+            isinstance(logger := self.logger, pl.loggers.WandbLogger)
+            and isinstance(logger.experiment, wandb.wandb_run.Run)
+            and (log_cfg := self.hparams.get("log", {}).get("validation", {}))
+        ):
+            self._tables = {}
+
+            if log_cfg.get(name := "outputs"):
+                columns = [
+                    f"{col}_{i}"
+                    for i in range(self.predict_steps)
+                    for col in ("speed_pred", "speed_gt")
+                ]
+                self._tables[name] = wandb.Table(columns=columns)
+
+    def on_validation_epoch_end(self):
+        if hasattr(self, "_tables") and self.trainer.state.stage != "sanity_check":
+            run: wandb.wandb_run.Run = self.logger.experiment  # type: ignore[union-attr]
+
+            if table := self._tables.get(name := "outputs"):
+                table.add_column("_step", list(map(int, table.get_index())))
+
+                frame_table: Optional[
+                    wandb.sdk.wandb_artifacts.ArtifactManifestEntry
+                ] = None
+
+                _table = (
+                    table
+                    if frame_table is None
+                    else wandb.JoinedTable(frame_table, table, "_step")
+                )
+
+                artifact = wandb.Artifact(f"run-{run.id}-val_{name}", "run_table")
+                artifact.add(_table, name)
+                run.log_artifact(artifact)
