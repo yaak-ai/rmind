fix: change gato decoder and add masking

cargpt/models/gato.py
@@ -79,11 +79,10 @@ class Gato(pl.LightningModule, LoadableFromArtifact):
         self.save_hyperparameters()
 
         self.encodings: ModuleDict = instantiate(self.hparams.encodings)
-        self.separator = torch.tensor([0.0])
 
         # for embeddings
-        self.discrete_cont_embedding = instantiate(
-            self.hparams.embeddings.discrete_cont_embedding  # type: ignore[union-attr]
+        self.discrete_embedding = instantiate(
+            self.hparams.embeddings.discrete_embedding  # type: ignore[union-attr]
         )
         self.local_position = instantiate(self.hparams.embeddings.local_position)  # type: ignore[union-attr]
         self.action_position = instantiate(self.hparams.embeddings.action_position)  # type: ignore[union-attr]
@@ -92,42 +91,70 @@ class Gato(pl.LightningModule, LoadableFromArtifact):
         self.transformer_decoder = instantiate(self.hparams.transformer_decoder)
         self.back_to_discrete = instantiate(self.hparams.back_to_discrete)
 
-        self.predict_steps: int = self.hparams.predict_steps  # type: ignore[assignment]
+        # Special tokens
+        self.separator_idx = 0
+        self.start_token_idx = 1
+        self.end_token_idx = 2
 
     def _step(
         self,
         speed,
         gas_pedal_normalized,
     ):
-        speed_encoded = self.encodings.continues_values(speed)  # type: ignore[operator]
-        gas_pedal_normalized_encoded = self.encodings.continues_values(  # type: ignore[operator]
+        speed_encoded: Int[Tensor, "b ts"] = self.encodings.continues_values(speed)  # type: ignore[operator]
+        gas_pedal_normalized_encoded: Int[Tensor, "b ts"] = self.encodings.continues_values(  # type: ignore[operator]
             gas_pedal_normalized
         )
 
         # when more input data is added - concat vectors and pass as tensors encoded
-        pred, tgt_discrete_actions = self.forward(
-            tensors_encoded=speed_encoded[..., :-1],
-            discrete_actions_encoded=gas_pedal_normalized_encoded[..., 1:],
+        tensors_encoded: Int[Tensor, "b ts n"] = rearrange(
+            speed_encoded[..., :-1], "b (ts 1) -> b ts 1"
+        )
+        discrete_actions_encoded: Int[Tensor, "b ts A"] = rearrange(
+            gas_pedal_normalized_encoded[..., 1:], "b (ts 1) -> b ts 1"
         )
 
-        return pred, tgt_discrete_actions
+        pred = self.forward(
+            tensors_encoded=tensors_encoded,
+            discrete_actions_encoded=discrete_actions_encoded,
+        )
 
-    def _compute_loss(self, predictions, tgt_discrete_actions):
+        b, _, _ = pred.shape
+        special_token = torch.ones(size=(b, 1)).to(tensors_encoded)
+        ground_truth: Int[Tensor, "b l"] = torch.cat(
+            [
+                special_token * self.start_token_idx,
+                tensors_encoded[:, -1],
+                special_token * self.separator_idx,
+                discrete_actions_encoded[:, -1],
+                special_token * self.end_token_idx,
+            ],
+            dim=1,
+        )
+        ground_truth_mask: Int[Tensor, "b l"] = torch.ones_like(ground_truth)
+        # The only thing we don't want to predict right now is separator
+        # TODO: let it be configurabe from cfg if possible
+        separator_position = tensors_encoded[:, -1].shape[1] + 1
+        ground_truth_mask[:, separator_position] = 0
+
+        return pred, ground_truth, ground_truth_mask
+
+    def _compute_loss(self, predictions, ground_truth, ground_truth_mask):
         log_p = log_softmax(predictions, dim=-1)
-        # TODO: mask tokens when more than speed is available -> m(b,l) (2.3, eq 2)
         loss = -(
             torch.gather(
                 input=log_p,
                 dim=-1,
-                index=tgt_discrete_actions[:, :, None].to(torch.int64),
-            )
+                index=ground_truth[:, :, None].to(torch.int64),
+            ).squeeze(-1)
+            * ground_truth_mask
         ).sum()
         return loss
 
     def training_step(self, batch, batch_idx):
         speed, gas_pedal_normalized = self.unpack_batch_for_predictions(batch)
-        pred, tgt_discrete_actions = self._step(speed, gas_pedal_normalized)
-        loss = self._compute_loss(pred, tgt_discrete_actions)
+        pred, ground_truth, ground_truth_mask = self._step(speed, gas_pedal_normalized)
+        loss = self._compute_loss(pred, ground_truth, ground_truth_mask)
         self.log(
             "train/loss",
             loss,
@@ -136,13 +163,14 @@ class Gato(pl.LightningModule, LoadableFromArtifact):
             prog_bar=True,
             sync_dist=True,
             rank_zero_only=True,
+            batch_size=speed.shape[0],
         )
         return loss
 
     def validation_step(self, batch, batch_idx):
         speed, gas_pedal_normalized = self.unpack_batch_for_predictions(batch)
-        pred, tgt_discrete_actions = self._step(speed, gas_pedal_normalized)
-        loss = self._compute_loss(pred, tgt_discrete_actions)
+        pred, ground_truth, ground_truth_mask = self._step(speed, gas_pedal_normalized)
+        loss = self._compute_loss(pred, ground_truth, ground_truth_mask)
         self.log(
             "val/loss",
             loss,
@@ -151,18 +179,25 @@ class Gato(pl.LightningModule, LoadableFromArtifact):
             prog_bar=True,
             sync_dist=True,
             rank_zero_only=True,
+            batch_size=speed.shape[0],
         )
 
         if hasattr(self, "_tables"):
-            speed_predicted: Float[Tensor, "b t"] = self.invert_predictions(pred)
-            speed_gt: Float[Tensor, "b t"] = speed[..., -speed_predicted.shape[-1] :]
+            inverted: Float[Tensor, "b t"] = self.invert_predictions(pred)
+            # TODO: figure out a more generic solution
+            gas_pedal_normalized_pred: Float[Tensor, "b"] = inverted[:, 3]
+            gas_pedal_normalized_gt: Float[Tensor, "b"] = gas_pedal_normalized[:, -1]
+            speed_pred: Float[Tensor, "b"] = inverted[:, 1]
+            speed_gt: Float[Tensor, "b"] = speed[:, -1]
             if table := self._tables.get("outputs"):
                 data: Float[Tensor, "b tx"] = rearrange(
                     [  # type: ignore
-                        speed_predicted,
+                        gas_pedal_normalized_pred,
+                        gas_pedal_normalized_gt,
+                        speed_pred,
                         speed_gt,
                     ],
-                    "x b t -> b (t x)",
+                    "x b -> b x",
                 )
 
                 for row in data.tolist():
@@ -182,71 +217,91 @@ class Gato(pl.LightningModule, LoadableFromArtifact):
     def forward(
         self,
         *,
-        tensors_encoded: Int[Tensor, "b t"],
-        discrete_actions_encoded: Int[Tensor, "b t"],
+        tensors_encoded: Int[Tensor, "b ts 1"],
+        discrete_actions_encoded: Int[Tensor, "b ts 1"],
     ):
+        b, ts, _ = tensors_encoded.shape
+
         # encodings to embeddings - learnable!
-        tensors_embeddings: Float[Tensor, "b t e"] = self.discrete_cont_embedding(
+        tensors_embeddings: Float[Tensor, "b ts 1 e"] = self.discrete_embedding(
             tensors_encoded
         )
-        b, t, e = tensors_embeddings.shape
 
         # construct observations and add local positional embedding
         observations = tensors_embeddings
-        positions = torch.range(0, t - 1, dtype=torch.int, device=observations.device)
-        positions_encoded = repeat(self.local_position(positions), "t e -> b t e", b=b)
+        _, _, observations_tokens, _ = observations.shape
+        positions = torch.arange(
+            0, observations_tokens, dtype=torch.int, device=observations.device
+        )
+        positions = self.local_position(positions)
+        positions_encoded = repeat(positions, "tokens e -> b ts tokens e", b=b, ts=ts)
         observations_embeddings = observations + positions_encoded
 
         # Prepare actions, use the same position always
-        actions_embeddings: Float[Tensor, "b t e"] = self.discrete_cont_embedding(
+        actions_embeddings: Float[Tensor, "b ts 1 e"] = self.discrete_embedding(
             discrete_actions_encoded
         )
         action_position_encoded: Float[Tensor, "1 e"] = self.action_position(
             torch.tensor([0], device=actions_embeddings.device)
         )
+        _, _, actions_tokens, _ = actions_embeddings.shape
         actions_embeddings = actions_embeddings + repeat(
-            action_position_encoded, "1 e -> b (t 1) e", b=b, t=t
+            action_position_encoded,
+            "1 e -> b ts (tokens 1) e",
+            b=b,
+            ts=ts,
+            tokens=actions_tokens,
         )
-
-        # Make a separator of a correct shape, it's always the same
-        separator = torch.zeros_like(actions_embeddings)
-
-        # Make split point: the first step from tgt is always visible to the network
-        # so the split is shifted by one
-        split_point = t - (self.predict_steps + 1)
+        # Get start and end token plus separator
+        start_token, end_token, separator = self.discrete_embedding(
+            torch.tensor(
+                [self.start_token_idx, self.end_token_idx, self.separator_idx],
+                device=actions_embeddings.device,
+            )
+        ).chunk(3, dim=0)
 
         # construct full sequence: [[o, |, a], [o, |, a], ...]
         # appendix B of the paper (https://arxiv.org/abs/2205.06175)
-        full_sequence: Float[Tensor, "b L e"] = rearrange(  # type: ignore[assignment]
+        full_sequence: Float[Tensor, "b ts L e"] = torch.concat(  # type: ignore[assignment]
             [
-                observations_embeddings[:, :split_point],
-                separator[:, :split_point],
-                actions_embeddings[:, :split_point],
+                observations_embeddings,
+                repeat(separator, "1 e -> b ts 1 e", b=b, ts=ts),
+                actions_embeddings,
             ],
-            "x b t e -> b (t x) e",
-        )  # this is like python's zip on the 1st dim
+            dim=2,  # concat tokens on each timestamp
+        )
 
-        # pass through decoder
-        tgt_seq = actions_embeddings[:, split_point:, ...]
-        _, m, _ = tgt_seq.shape
-        tgt_mask = torch.tril(torch.ones(m, m, device=tgt_seq.device)).float()
+        # The last timestep s_l = [o_l | a_l] from full sequence is the one the decoder
+        # should predict
+        tgt_seq: Float[Tensor, "b L e"] = full_sequence[:, -1:].squeeze(1)
+        tgt_seq = torch.concat(
+            [
+                repeat(start_token, "1 e -> b 1 e", b=b),
+                tgt_seq,
+                repeat(end_token, "1 e -> b 1 e", b=b),
+            ],
+            dim=1,
+        )
+        _, seq_tokens, _ = tgt_seq.shape
+        tgt_mask = torch.tril(
+            torch.ones(seq_tokens, seq_tokens, device=tgt_seq.device)
+        ).float()
+        # mask out separator which is just after observations
+        separator_idx = observations_tokens + 1
+        tgt_mask[separator_idx, separator_idx] = 0.0
         tgt_mask = tgt_mask.masked_fill(tgt_mask == 0.0, -torch.inf)
 
         pred = self.transformer_decoder(
             tgt=tgt_seq,
-            memory=full_sequence,
+            memory=rearrange(full_sequence[:, :-1], "b ts L e -> b (ts L) e"),
             tgt_mask=tgt_mask,
         )
         pred = self.back_to_discrete(pred)
-
-        # Reverse shift from split point
-        pred = pred[:, :-1, :]
-        discrete_actions_encoded = discrete_actions_encoded[:, (split_point + 1) :]
-        return pred, discrete_actions_encoded
+        return pred
 
     def unpack_batch_for_predictions(
         self, batch
-    ) -> Tuple[Float[Tensor, "b t"], Float[Tensor, "b t"],]:
+    ) -> Tuple[Float[Tensor, "b t"], Float[Tensor, "b t"]]:
         clips = mit.one(batch["clips"].values())
         meta = clips["meta"]
 
@@ -280,9 +335,10 @@ class Gato(pl.LightningModule, LoadableFromArtifact):
 
             if log_cfg.get(name := "outputs"):
                 columns = [
-                    f"{col}_{i}"
-                    for i in range(self.predict_steps)
-                    for col in ("speed_pred", "speed_gt")
+                    "gas_pedal_normalized_pred",
+                    "gas_pedal_normalized_gt",
+                    "speed_pred",
+                    "speed_gt",
                 ]
                 self._tables[name] = wandb.Table(columns=columns)
 