20706bc469a9468a0e0b8781931a3d9017f6efbb
Anna Gut <anna@yaak.ai>
2023-07-06 16:54:28 +0200

    refactor

--- cargpt/utils/_wandb.py
+++ cargpt/utils/_wandb.py
@@ -131,9 +131,9 @@ class TrainValAttnMapLoggingMixin:
     metadata_keys: List[str]
     sensor_detokenization: Dict[str, Callable]
 
-    def is_attn_map_logging_active(self):
+    def __is_logging_active(self):
         try:
-            self._get_attn_map_logging_params()
+            self.__get_params()
         except KeyError:
             params_fine = False
         else:
@@ -145,8 +145,8 @@ class TrainValAttnMapLoggingMixin:
             and isinstance(logger.experiment, Run)
         )
 
-    def _should_attn_map_log_now(self, batch_idx):
-        params: Dict[str, Any] = self._get_attn_map_logging_params()
+    def __should_log_now(self, batch_idx):
+        params: Dict[str, Any] = self.__get_params()
         if self.trainer.state.stage == RunningStage.TRAINING:
             curr_step = self.global_step
         else:  # validation
@@ -155,7 +155,7 @@ class TrainValAttnMapLoggingMixin:
         log_freq = params.get("log_freq") or 50
         return curr_step % log_freq == 0
 
-    def _get_attn_map_logging_params(self):
+    def __get_params(self):
         params: Dict[str, Any] = self.hparams["log"]
         if self.trainer.state.stage == RunningStage.TRAINING:
             params = params["training"]["attention_maps"]
@@ -165,7 +165,7 @@ class TrainValAttnMapLoggingMixin:
             raise KeyError("wrong stage")
         return params
 
-    def get_attention_maps(self, x, mask=None):
+    def __get_attention_maps(self, x, mask=None):
         attention_maps = []
         with torch.no_grad():
             for layer in self.gpt.layers:  # type: ignore
@@ -177,7 +177,7 @@ class TrainValAttnMapLoggingMixin:
                 x = layer(x)
         return attention_maps
 
-    def _logits_to_real(self, logits: Float[Tensor, "to e"]) -> List[float]:
+    def __logits_to_real(self, logits: Float[Tensor, "to e"]) -> List[float]:
         action_logits = logits[-len(self.action_keys) :, ...]
         tokens = torch.argmax(torch.softmax(action_logits, dim=-1), dim=-1)
 
@@ -201,22 +201,22 @@ class TrainValAttnMapLoggingMixin:
         episode_mask: Optional[Float[Tensor, "to to"]] = None,
         **kwargs,
     ):
-        if not self.is_attn_map_logging_active():
+        if not self.__is_logging_active():
             return
 
-        if not self._should_attn_map_log_now(batch_idx):
+        if not self.__should_log_now(batch_idx):
             return
 
         # Pick only one sample from batch - consistent with deephouse approach
         batch_idx = 0
 
-        attn_maps: List[Float[Tensor, "to to"]] = self.get_attention_maps(
+        attn_maps: List[Float[Tensor, "to to"]] = self.__get_attention_maps(
             episode[batch_idx],
             episode_mask,
         )
 
         batch_frames: Float[Tensor, "ts c h w"] = frames[batch_idx]
-        images, metas_actions = self._postprocess_attn_maps(
+        images, metas_actions = self.__postprocess_attn_maps(
             attn_maps=attn_maps,
             frames=batch_frames,
             tokens=episode.shape[1],
@@ -226,8 +226,185 @@ class TrainValAttnMapLoggingMixin:
         drive_id = drive_ids[batch_idx]
         frame_idxs = frame_idxs[batch_idx].tolist()
         gt_actions_values = episode_values[batch_idx, -len(self.action_keys) :].tolist()
-        pred_actions_values = self._logits_to_real(logits[batch_idx])
+        pred_actions_values = self.__logits_to_real(logits[batch_idx])
 
+        stage = self.trainer.state.stage
+        data: Dict[str, Any] = self.__get_image_data(
+            gt_actions_values=gt_actions_values,
+            pred_actions_values=pred_actions_values,
+            frame_idxs=frame_idxs,
+            drive_id=drive_id,
+            prefix="img",
+            images=images,
+        )
+        data.update(
+            self.__get_image_data(
+                gt_actions_values=gt_actions_values,
+                pred_actions_values=pred_actions_values,
+                frame_idxs=frame_idxs,
+                drive_id=drive_id,
+                prefix="meta+actions",
+                images=metas_actions,
+            )
+        )
+        data.update(
+            {
+                f"{stage}/attn_map/frames": [
+                    wandb.Image(img, caption=f"{frame_idxs[idx]} [{drive_id}]")
+                    for idx, img in enumerate(batch_frames)
+                ]
+            }
+        )
+        data.update(self.__get_histograms_data(attn_maps=attn_maps))
+        # Don't pass step, don't commit - let it be committed with logs
+        params = self.__get_params()
+        commit = params.get("force_commit") or False
+        self.logger.experiment.log(data=data, commit=commit)
+
+    def __postprocess_attn_maps(
+        self,
+        attn_maps: List[Float[Tensor, "to to"]],
+        frames: Float[Tensor, "ts c h w"],
+        tokens: int,
+    ) -> Tuple[List[List[Image]], List[List[Image]]]:
+        # Extract and prepare parameters
+        params = self.__get_params()
+
+        mean = params["imgs_mean"]
+        std = params["imgs_std"]
+        unnormalize = Unnormalize(mean=mean, std=std)
+        frames = unnormalize(frames.clone())
+
+        # Extract and calculate dimensions
+        timesteps, _, img_height, img_width = frames.shape
+        meta_sep_act_len = len(self.action_keys) + 1 + len(self.metadata_keys)
+        seq_len = (tokens + 1) // timesteps
+        images_len = seq_len - meta_sep_act_len
+        cols = params["cols"]
+        rows = params["rows"]
+
+        last_meta_actions = self.__extract_single_attn_map(attn_maps)
+
+        # Split attention map into multiple images
+        images = []
+        metas_actions = []
+        for idx, row in enumerate(last_meta_actions):
+            row_images: List[Image] = []
+            row_metas_actions: List[Image] = []
+
+            row = self.__preprocess_attn_row(row_vector=row)
+
+            for ts in range(timesteps):
+                # Get frame attention mixed with real frame
+                img_start_idx = ts * seq_len
+                img_end_idx = img_start_idx + images_len
+                mask = row[img_start_idx:img_end_idx].reshape(rows, cols).clone()
+                row_images.append(
+                    fromarray(self.__get_masked_img(mask=mask, frame=frames[ts]))
+                )
+
+                # Get non-image tokens visualization
+                ma_start_idx = img_end_idx
+                ma_end_idx = ma_start_idx + meta_sep_act_len
+                ma = row[ma_start_idx:ma_end_idx]
+                row_metas_actions.append(
+                    fromarray(
+                        self.__get_meta_and_actions_img(
+                            meta_actions=ma, length=meta_sep_act_len
+                        )
+                    )
+                )
+
+            images.append(row_images)
+            metas_actions.append(row_metas_actions)
+
+        return images, metas_actions
+
+    def __extract_single_attn_map(
+        self,
+        attn_maps: List[Float[Tensor, "to to"]],
+    ) -> Float[Tensor, "a to"]:
+        params = self.__get_params()
+        layer = params["layer"]
+        attn_map = attn_maps[layer].clone()
+        last_meta_actions = attn_map[-len(self.action_keys) :]
+        return last_meta_actions
+
+    def __preprocess_attn_row(
+        self, row_vector: Float[Tensor, "to"]
+    ) -> Float[Tensor, "to"]:
+        params = self.__get_params()
+        transforms = params.get("row_transform")
+        out = row_vector.clone()
+        for transform_cfg in transforms:
+            transform: Callable = instantiate(transform_cfg)
+            out = transform(out)
+        return out
+
+    def __get_masked_img(self, mask, frame):
+        _, img_height, img_width = frame.shape
+        params = self.__get_params()
+        strength = params.get("strength") or 1.0
+        reverse = params.get("reverse") or False
+
+        color = params.get("color") or [255, 255, 255]
+        color = rearrange(torch.tensor(color).to(frame) / 255.0, "(c 1 1) -> c 1 1")
+
+        mask = torch.nn.functional.interpolate(
+            input=mask[None, None, ...],
+            size=(img_height, img_width),
+            mode="bilinear",
+            align_corners=False,
+        ).squeeze(0)
+
+        if reverse:
+            mask = 1 - mask
+        mask = mask * strength
+
+        background = (1.0 - mask) * frame
+        foreground = torch.ones_like(background) * color * mask
+        img = background + foreground
+
+        img = (img.permute(1, 2, 0) * 255).int().cpu().numpy().astype(np.uint8)
+
+        return img
+
+    def __get_meta_and_actions_img(self, meta_actions, length):
+        ma = meta_actions
+        if len(ma) < length:
+            tmp = torch.zeros(length).to(ma)
+            tmp[: len(ma)] = ma
+            ma = tmp
+        ma = ma[None, ...]
+        ma = (ma * 255).int().cpu().numpy().astype(np.uint8)
+        return ma
+
+    def __get_histograms_data(
+        self, attn_maps: List[Float[Tensor, "a to"]]
+    ) -> Dict[str, wandb.Histogram]:
+        params = self.__get_params()
+        histograms = params.get("histograms") or False
+        if not histograms:
+            return {}
+
+        last_meta_actions = self.__extract_single_attn_map(attn_maps)
+        data = {}
+        for idx, vector in enumerate(last_meta_actions):
+            weights = vector.clone()
+            histogram = wandb.Histogram(weights.detach().cpu(), num_bins=512)  # type: ignore
+            key = f"{self.trainer.state.stage}-histograms/{self.action_keys[idx]}"
+            data[key] = histogram
+        return data
+
+    def __get_image_data(
+        self,
+        gt_actions_values,
+        pred_actions_values,
+        frame_idxs,
+        drive_id,
+        prefix,
+        images,
+    ) -> Dict[str, List[wandb.Image]]:
         stage = self.trainer.state.stage
         data = {
             f"{stage}/attn_map/{prefix}-{self.action_keys[idx]}": [
@@ -241,126 +418,6 @@ class TrainValAttnMapLoggingMixin:
                 )
                 for frame_idx, img in enumerate(ts_images)
             ]
-            for prefix, values in (("img", images), ("meta+actions", metas_actions))
-            for idx, ts_images in enumerate(values)
+            for idx, ts_images in enumerate(images)
         }
-        data.update(
-            {
-                f"{stage}/attn_map/frames": [
-                    wandb.Image(img, caption=f"{frame_idxs[idx]} [{drive_id}]")
-                    for idx, img in enumerate(batch_frames)
-                ]
-            }
-        )
-        # Don't pass step, don't commit - let it be committed with logs
-        params = self._get_attn_map_logging_params()
-        commit = params.get("force_commit") or False
-        self.logger.experiment.log(data=data, commit=commit)
-
-    def _postprocess_attn_maps(
-        self,
-        attn_maps: List[Float[Tensor, "to to"]],
-        frames: Float[Tensor, "ts c h w"],
-        tokens: int,
-    ) -> Tuple[List[List[Image]], List[List[Image]]]:
-        # Extract and prepare parameters
-        params = self._get_attn_map_logging_params()
-        layer = params["layer"]
-        strength = params.get("strength") or 1.0
-        reverse = params.get("reverse") or False
-        row_transform = params.get("row_transform")
-        histograms = params.get("histograms") or False
-
-        color = params.get("color") or [255, 255, 255]
-        color = rearrange(torch.tensor(color).to(frames) / 255.0, "(c 1 1) -> c 1 1")
-
-        mean = params["imgs_mean"]
-        std = params["imgs_std"]
-        unnormalize = Unnormalize(mean=mean, std=std)
-        frames = unnormalize(frames.clone())
-
-        # Extract and calculate dimensions
-        timesteps, _, img_height, img_width = frames.shape
-        actions_len = len(self.action_keys)
-        metadata_len = len(self.metadata_keys)
-        meta_sep_act_len = actions_len + 1 + metadata_len
-        seq_len = (tokens + 1) // timesteps
-        images_len = seq_len - meta_sep_act_len
-        cols = params["cols"]
-        rows = params["rows"]
-
-        attn_map = attn_maps[layer].clone()
-        last_meta_actions = attn_map[-actions_len:]
-
-        # Split attention map into multiple images
-        images = []
-        metas_actions = []
-        for idx, row in enumerate(last_meta_actions):
-            row_images: List[Image] = []
-            row_metas_actions: List[Image] = []
-
-            if histograms:
-                self._process_attn_row_as_histogram(
-                    row, action_key=self.action_keys[idx]
-                )
-
-            row = self._preprocess_attn_row(row_vector=row, transforms=row_transform)
-
-            for ts in range(timesteps):
-                # Get frame attention mixed with real frame
-                img_start_idx = ts * seq_len
-                img_end_idx = img_start_idx + images_len
-                mask = row[img_start_idx:img_end_idx].reshape(rows, cols).clone()
-                mask = torch.nn.functional.interpolate(
-                    input=mask[None, None, ...],
-                    size=(img_height, img_width),
-                    mode="bilinear",
-                    align_corners=False,
-                ).squeeze(0)
-
-                if reverse:
-                    mask = 1 - mask
-                mask = mask * strength
-
-                background = (1.0 - mask) * frames[ts]
-                foreground = torch.ones_like(background) * color * mask
-                img = background + foreground
-
-                img = (img.permute(1, 2, 0) * 255).int().cpu().numpy().astype(np.uint8)
-                row_images.append(fromarray(img))
-
-                # Get non-image tokens visualization
-                ma_start_idx = img_end_idx
-                ma_end_idx = ma_start_idx + meta_sep_act_len
-                ma = row[ma_start_idx:ma_end_idx]
-                if len(ma) < meta_sep_act_len:
-                    tmp = torch.zeros(meta_sep_act_len).to(ma)
-                    tmp[: len(ma)] = ma
-                    ma = tmp
-                ma = ma[None, ...]
-                ma = (ma * 255).int().cpu().numpy().astype(np.uint8)
-                row_metas_actions.append(fromarray(ma))
-
-            images.append(row_images)
-            metas_actions.append(row_metas_actions)
-
-        return images, metas_actions
-
-    def _preprocess_attn_row(
-        self, row_vector: Float[Tensor, "to"], transforms: Iterable[Any]
-    ) -> Float[Tensor, "to"]:
-        out = row_vector.clone()
-        for transform_cfg in transforms:
-            transform: Callable = instantiate(transform_cfg)
-            out = transform(out)
-        return out
-
-    def _process_attn_row_as_histogram(
-        self, row_vector: Float[Tensor, "to"], action_key: str
-    ) -> None:
-        weights = row_vector.clone()
-        histogram = wandb.Histogram(weights.detach().cpu(), num_bins=512)  # type: ignore
-        self.logger.experiment.log(
-            data={f"{self.trainer.state.stage}-histograms/{action_key}": histogram},
-            commit=False,
-        )
+        return data
