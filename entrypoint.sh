f1c238de2cdb1415d69653d1fcdcb283a2a9f55f
Harsimrat Sandhawalia <harsimrat+github@gmail.com>
2023-12-14 17:04:06 +0100

    flags

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,14 +13,17 @@ help_message="¿Que? Please specify --train to run model training or --predict t
 
 . parse_args.sh
 
+echo train=$train predict=$predict dvc=$dvc
+
 # Actual entrypoint
-if [ $train ]; then
+if [ $train = true ]; then
   echo WANDB_MODE=$WANDB_MODE WAND_API_KEY=$WAND_API_KEY python train.py $flags
   WANDB_MODE=$WANDB_MODE WAND_API_KEY=$WAND_API_KEY python train.py $flags
-elif [ $predict ]; then
+elif [ $predict = true ]; then
   echo WANDB_MODE=$WANDB_MODE WAND_API_KEY=$WAND_API_KEY python predict.py $flags
   WANDB_MODE=$WANDB_MODE WAND_API_KEY=$WAND_API_KEY python predict.py $flags
-elif [ $dvc ]; then
+elif [ $dvc = true ]; then
+  echo dvc repro
   AWS_ACCESS_KEY_ID=$aws_access_key_id AWS_SECRET_ACCESS_KEY=$aws_secret_access_key dvc repro
 else
   echo "¿Neither train, predict or dvc?"
