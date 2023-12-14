75a4873168d2bbe7420cc7c00e90f6e7169aeacf
Harsimrat Sandhawalia <harsimrat+github@gmail.com>
2023-12-14 15:50:52 +0100

    add dvc image

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,11 @@
 
 train=false
 predict=false
+dvc=false
 WANDB_MODE=offline
 WAND_API_KEY=NA
+aws_access_key_id=
+aws_secret_access_key=
 flags="--help"
 
 help_message="¿Que? Please specify --train to run model training or --predict to one-off inference"
@@ -12,9 +15,13 @@ help_message="¿Que? Please specify --train to run model training or --predict t
 
 # Actual entrypoint
 if [ $train ]; then
+  echo WANDB_MODE=$WANDB_MODE WAND_API_KEY=$WAND_API_KEY python train.py $flags
   WANDB_MODE=$WANDB_MODE WAND_API_KEY=$WAND_API_KEY python train.py $flags
 elif [ $predict ]; then
+  echo WANDB_MODE=$WANDB_MODE WAND_API_KEY=$WAND_API_KEY python predict.py $flags
   WANDB_MODE=$WANDB_MODE WAND_API_KEY=$WAND_API_KEY python predict.py $flags
+elif [ $dvc ]; then
+  AWS_ACCESS_KEY_ID=$aws_access_key_id AWS_SECRET_ACCESS_KEY=$aws_secret_access_key dvc repro
 else
-  echo "¿Neither train nor predict?"
+  echo "¿Neither train, predict or dvc?"
 fi
