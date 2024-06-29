8d6183a635a9fd5abfc86d0f5e754c91597e72c3
Harsimrat Sandhawalia <harsimrat+github@gmail.com>
2024-06-29 11:06:06 +0200

    Faiss index build script

---
+++ cargpt/callbacks/embedding_writer.py
@@ -0,0 +1,44 @@
+from collections.abc import Sequence
+from pathlib import Path
+from typing import Any
+
+import more_itertools as mit
+import pytorch_lightning as pl
+import torch
+from pytorch_lightning.callbacks import BasePredictionWriter
+from tensordict import TensorDict
+from typing_extensions import override
+
+
+class EmbeddingWriter(BasePredictionWriter):
+    def __init__(self, output_dir: str | Path) -> None:
+        super().__init__(write_interval="batch")
+        self.output_dir = Path(output_dir)
+
+    @override
+    def write_on_batch_end(
+        self,
+        trainer: pl.Trainer,
+        pl_module: pl.LightningModule,
+        prediction: TensorDict,
+        batch_indices: Sequence[int] | None = None,
+        batch: Any = None,
+        batch_idx: int = 0,
+        dataloader_idx: int = 0,
+    ) -> None:
+        camera_name = mit.one(batch.frames.keys())
+        meta = batch.meta
+        drive_ids = meta.pop("drive_id").detach()
+        drive_ids = [
+            drive_id.type(torch.uint8).cpu().numpy().tobytes().decode("ascii").strip()
+            for drive_id in drive_ids
+        ]
+        frame_idxs = meta[f"{camera_name}/ImageMetadata_frame_idx"].tolist()
+
+        obj = {
+            "features": prediction.detach().cpu(),
+            "frame_idx": frame_idxs,
+            "drive_id": drive_ids,
+            "camera_name": camera_name,
+        }
+        torch.save(obj, f"{self.output_dir}/{batch_idx:06}.pt")
---
+++ cargpt/utils/embeddings.py
@@ -0,0 +1,71 @@
+from typing import Any
+
+import pytorch_lightning as pl
+from hydra.utils import instantiate
+from jaxtyping import Float  # noqa: TCH002
+from omegaconf import DictConfig
+from tensordict import TensorDict
+from torch import Tensor  # noqa: TCH002
+from typing_extensions import override
+from yaak_datasets import Batch
+
+from cargpt.components.episode import Modality, SpecialToken
+
+
+class Embeddings(pl.LightningModule):
+    def __init__(self, base: DictConfig):
+        super().__init__()
+        self.base = instantiate(base)
+
+    @override
+    def predict_step(
+        self, batch: Any, batch_idx: int, dataloader_idx: int = 0
+    ) -> TensorDict:
+        return self.embeddings_step(batch)
+
+    def embeddings_step(self, batch: Batch) -> TensorDict:  # pyright: ignore[reportGeneralTypeIssues]
+        inputs = self.base._build_input(batch)
+        episode = self.base.episode_builder.build_episode(inputs)
+
+        embeddigs = TensorDict({}, batch_size=[], device=inputs.device)
+
+        for name, objective in self.base.objectives.items():
+            summaries = self.summary_embeddings(objective, episode)
+            for token_name, embedding in summaries.items():
+                embeddigs[f"{name}/{token_name}"] = embedding
+
+        return embeddigs
+
+    def summary_embeddings(self, objective, episode) -> TensorDict:
+        mask = objective._build_attention_mask(episode.index, episode.timestep)
+        embedding = self.base.encoder(src=episode.packed_embeddings, mask=mask.data)
+
+        index = episode.index[[-1]]
+
+        observation_summary: Float[Tensor, "b t 1 d"] = (
+            index.select(k := (Modality.SPECIAL, SpecialToken.OBSERVATION_SUMMARY))
+            .parse(embedding)
+            .get(k)
+        )
+
+        observation_history: Float[Tensor, "b t 1 d"] = (
+            index.select(k := (Modality.SPECIAL, SpecialToken.OBSERVATION_HISTORY))
+            .parse(embedding)
+            .get(k)
+        )
+
+        action_summary: Float[Tensor, "b t 1 d"] = (
+            index.select(k := (Modality.SPECIAL, SpecialToken.ACTION_SUMMARY))
+            .parse(embedding)
+            .get(k)
+        )
+
+        return TensorDict(
+            {
+                SpecialToken.OBSERVATION_SUMMARY: observation_summary,
+                SpecialToken.ACTION_SUMMARY: action_summary,
+                SpecialToken.OBSERVATION_HISTORY: observation_history,
+            },
+            batch_size=[],
+            device=embedding.device,
+        )
