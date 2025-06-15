7ab504686a8e7a3bcdc2c83c535439a7b3a2b4a0
sandhawalia <hs.sandhawalia@gmail.com>
2025-06-15 13:49:53 +0200

    moving to torch

--- src/rmind/components/llm.py
+++ src/rmind/components/llm.py
@@ -3,32 +3,79 @@ from typing import Any, Literal, final, override
 import torch
 from einops import rearrange, repeat
 from torch import Tensor, nn
-from xformers.components import Activation, ResidualNormStyle, build_activation
-from xformers.components.attention import ScaledDotProduct
-from xformers.components.attention.core import scaled_query_key_softmax
-from xformers.components.feedforward import register_feedforward
-from xformers.components.feedforward.mlp import Feedforward, MlpConfig
-from xformers.components.multi_head_dispatch import (
-    _fold_heads,  # pyright: ignore[reportPrivateUsage]  # noqa: PLC2701
-    _split_heads,  # pyright: ignore[reportPrivateUsage]  # noqa: PLC2701
-)
-from xformers.components.reversible import ReversibleSequence
-from xformers.factory.model_factory import (
-    get_weight_init_fn,
-    xFormerEncoderBlock,
-    xFormerEncoderConfig,
-    xFormerWeightInit,
-)
+from torch.utils.checkpoint import checkpoint
+
+class TransformerEncoderBlock(nn.Module):
+    def __init__(self, embedding_dim, num_heads, attn_dropout=0.1, resid_dropout=0.1, ffn_dropout=0.1, activation="gelu", hidden_layer_multiplier=1):
+        super().__init__()
+
+        # f
+        self.pre_norm = nn.LayerNorm(embedding_dim)  # pre-norm
+
+        self.self_attn = nn.MultiheadAttention(
+            embed_dim=embedding_dim, num_heads=num_heads, dropout=attn_dropout, batch_first=True
+        )
+
+        self.resid_drop = nn.Dropout(resid_dropout, inplace=False)
+
+        # g
+        self.post_norm = nn.LayerNorm(embedding_dim)  # ffn
+
+        self.mlp = MLPGLU(
+                dim_model=embedding_dim,
+                dropout=ffn_dropout,
+                activation="gelu",
+                hidden_layer_multiplier=hidden_layer_multiplier,
+            )
+
+    def forward(self, x, attn_mask=None):
+
+        # f
+        residual = x
+        x_norm = self.pre_norm(x)
+        mha, _ = self.self_attn(x_norm, x_norm, x_norm, attn_mask=attn_mask)
+        x = residual + self.resid_drop(mha)
+
+        # g
+        residual = x
+        y = residual + self.mlp(self.post_norm(x))
+
+        return y
 
 
-@final
-@register_feedforward("MLPGLU", MlpConfig)
-class MLPGLU(Feedforward):
+class TransformerEncoder(nn.Module):
+    def __init__(self, dim_model, num_layers, num_heads, attn_dropout=0.1, resid_dropout=0.1, ffn_dropout=0.1, hidden_layer_multiplier=1):
+        super().__init__()
+        self.layers = nn.ModuleList([
+            TransformerEncoderBlock(
+                embedding_dim=dim_model,
+                num_heads=num_heads,
+                attn_dropout=attn_dropout,
+                ffn_dropout=ffn_dropout,
+                resid_dropout=resid_dropout,
+                hidden_layer_multiplier=hidden_layer_multiplier
+            ) for _ in range(num_layers)
+        ])
+        # https://github.com/karpathy/nanoGPT/blob/master/model.py#L182
+        self.layer_norm = nn.LayerNorm(dim_model)
+
+
+    def forward(self, x, attn_mask=None):
+
+        for layer in self.layers:
+            if self.training:
+                x = checkpoint(layer, x, attn_mask)
+            else:
+                x = layer(x, attn_mask)
+
+        return self.layer_norm(x)
+
+
+class MLPGLU(nn.Module):
     def __init__(
         self,
         dim_model: int,
         dropout: float,
-        activation: str,
         hidden_layer_multiplier: int,
         bias: bool = True,  # noqa: FBT001, FBT002
         *_args: Any,
@@ -37,10 +84,9 @@ class MLPGLU(Feedforward):
         super().__init__()
         dim_mlp = hidden_layer_multiplier * dim_model
         self.l1 = nn.Linear(in_features=dim_model, out_features=dim_mlp * 2, bias=bias)
-        self.a1 = build_activation(Activation(activation))
+        self.a1 = nn.GELU()
         self.d1 = nn.Dropout(dropout)
         self.l2 = nn.Linear(in_features=dim_mlp, out_features=dim_model, bias=bias)
-        self.d2 = nn.Dropout(dropout)
 
     @override
     def forward(self, input: Tensor) -> Tensor:
@@ -49,125 +95,3 @@ class MLPGLU(Feedforward):
         xw, xv = x.chunk(2, dim=-1)
         geglu = self.a1(xw) * xv
         return self.l2(self.d1(geglu))
-
-
-class xFormerEncoder(nn.Module):  # noqa: N801
-    def __init__(
-        self,
-        *,
-        config: xFormerEncoderConfig,
-        weight_init: xFormerWeightInit = xFormerWeightInit.ViT,
-        freeze: bool | None = None,
-    ) -> None:
-        super().__init__()
-
-        if any((
-            not config.reversible,
-            config.residual_norm_style is ResidualNormStyle.DeepNorm,
-            config.position_encoding_config is not None,
-        )):
-            raise NotImplementedError
-
-        self.encoders: ReversibleSequence = ReversibleSequence(
-            nn.ModuleList(
-                nn.ModuleList(xFormerEncoderBlock.get_reversible_layer(config))
-                for _ in range(config.num_layers)
-            )
-        )
-        self.layer_norm: nn.LayerNorm = nn.LayerNorm(config.dim_model)
-
-        init_fn = get_weight_init_fn(weight_init)
-        for name, module in self.encoders.named_children():
-            init_fn(module=module, name=name, gain=1.0)
-
-        if freeze is not None:
-            self.requires_grad_(not freeze).train(not freeze)  # pyright: ignore[reportUnusedCallResult]
-
-    @override
-    def forward(self, src: Tensor, mask: Tensor) -> Tensor:
-        x = torch.cat([src, src], dim=-1)
-        x = self.encoders(x, att_mask=mask)
-        x = torch.stack(x.chunk(2, dim=-1))
-        x = x.mean(dim=0)
-
-        return self.layer_norm(x)
-
-    def compute_attention_rollout(
-        self,
-        src: Tensor,
-        mask: Tensor,
-        *,
-        head_fusion: Literal["max", "min", "mean"] = "max",
-        drop_ratio: float | None = None,
-    ) -> Tensor:
-        """[1] Quantifying Attention Flow in Transformers (https://arxiv.org/abs/2005.00928)
-        [2] Exploring Explainability for Vision Transformers (https://jacobgil.github.io/deeplearning/vision-transformer-explainability).
-        """
-        b, s, _ = src.shape
-        identity = torch.eye(s, s, device=mask.device)
-        attn_rollout = repeat(identity, "s_from s_to -> b s_from s_to", b=b)
-
-        seq = repeat(src, "b s d -> b s (n d)", n=2)
-        for block in self.encoders.blocks:
-            match (net := block.f.net).sublayer.attention:
-                case ScaledDotProduct():
-                    _, x = rearrange(seq, "b s (n d) -> n b s d", n=2)
-                    x = net.norm(x)
-                    q = net.sublayer.in_proj_container.q_proj(x)
-                    k = net.sublayer.in_proj_container.k_proj(x)
-
-                    # https://github.com/facebookresearch/xformers/blob/184b2808a5eed5f03ce381b8e7bd73f6188d8453/xformers/components/multi_head_dispatch.py#L236
-                    reshape_fn = (
-                        _split_heads
-                        if net.sublayer.attention.requires_head_dimension
-                        else _fold_heads
-                    )
-
-                    q = reshape_fn(
-                        q, b, s, net.sublayer.num_heads, net.sublayer.dim_key_head
-                    )
-                    k = reshape_fn(
-                        k, b, s, net.sublayer.num_heads, net.sublayer.dim_key_head
-                    )
-
-                    attn = scaled_query_key_softmax(q, k, att_mask=mask)
-
-                    # unfold attention
-                    attn = attn.view(b, net.sublayer.num_heads, s, s)
-
-                    match head_fusion:
-                        case "mean":
-                            attn = attn.mean(axis=1)  # pyright: ignore[reportCallIssue]
-                        case "max":
-                            attn = attn.max(axis=1)[0]  # pyright: ignore[reportCallIssue]
-                        case "min":
-                            attn = attn.min(axis=1)[0]  # pyright: ignore[reportCallIssue]
-
-                    # TODO: exclude certain indices from getting dropped?  # noqa: FIX002
-                    if drop_ratio is not None:
-                        attn_flat = rearrange(attn, "b s_from s_to -> (b s_from) s_to)")
-                        drop_count = int(attn_flat.shape[-1] * drop_ratio)
-                        drop_indices = attn_flat.topk(
-                            k=drop_count, dim=-1, largest=False
-                        ).indices
-                        for batch_idx, batch_drop_indices in enumerate(drop_indices):
-                            attn_flat[batch_idx, batch_drop_indices] = 0
-
-                        attn = rearrange(
-                            attn_flat,
-                            "(b s_from) s_to -> b s_from s_to",
-                            s_from=s,
-                            s_to=s,
-                        )
-
-                    attn = (attn + identity) * 0.5
-                    # [2] > We also have to normalize the rows, to keep the total attention flow 1.
-                    attn /= attn.sum(dim=-1, keepdim=True)
-                    attn_rollout = torch.matmul(attn, attn_rollout)
-
-                case _:
-                    raise NotImplementedError
-
-            seq = block(seq, f_args={"att_mask": mask})
-
-        return attn_rollout
