c51d2c13eaaa9cb29b4ee9294cbbd6b35db8dc34
koritcky <koritcky@gmail.com>
2025-08-29 14:08:35 +0200

    add non_zero_signal_with_threshold

--- src/rmind/utils/functional.py
+++ src/rmind/utils/functional.py
@@ -1,5 +1,5 @@
 from functools import partial
-from typing import Any
+from typing import Any, NamedTuple
 
 import more_itertools as mit
 import torch
@@ -37,3 +37,21 @@ def diff_last(input: Tensor, n: int = 1, *, append: float | None = None) -> Tens
         else None
     )
     return torch.diff(input, n=n, dim=-1, append=append_)
+
+
+class SignalWithThresholdResult(NamedTuple):
+    class_idx: Tensor
+    prob: Tensor
+
+
+def non_zero_signal_with_threshold(
+    logits: Tensor, threshold: float = 0.85
+) -> SignalWithThresholdResult:
+    # assuming zero signal is at index 0
+    probs = F.softmax(logits, dim=-1)
+    max_prob_interest, max_idx_relative = torch.max(probs[..., 1:], dim=-1)
+    final_class_idx = torch.where(
+        max_prob_interest > threshold, max_idx_relative + 1, 0
+    )
+    final_prob = torch.gather(probs, -1, final_class_idx.unsqueeze(-1)).squeeze(-1)
+    return SignalWithThresholdResult(class_idx=final_class_idx, prob=final_prob)
