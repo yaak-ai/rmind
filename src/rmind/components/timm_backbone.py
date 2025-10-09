0819fc43ba9b5ef29d373f69e1b5ee597dfbdb37
koritsky <koritcky@gmail.com>
2025-10-09 16:52:47 +0200

    WIP

---
+++ src/rmind/components/timm_backbone.py
@@ -0,0 +1,29 @@
+from math import prod
+from typing import override
+
+from timm import create_model
+from torch import Tensor, nn
+
+
+class TimmBackbone(nn.Module):
+    def __init__(
+        self,
+        model_name: str = "vit_small_patch16_dinov3.lvd1689m",
+        *,
+        freeze: bool | None = None,
+    ) -> None:
+        super().__init__()
+        self.model: nn.Module = create_model(
+            model_name, pretrained=True, features_only=True
+        )
+
+        if freeze is not None:
+            self.requires_grad_(not freeze).train(not freeze)  # pyright: ignore[reportUnusedCallResult]
+
+    @override
+    def forward(self, input: Tensor) -> Tensor:
+        *b, c, h, w = input.shape
+        x = input.view(prod(b), c, h, w)
+        x = self.model(x)[0]
+        *_, c, h, w = x.shape
+        return x.view(*b, c, h, w)
