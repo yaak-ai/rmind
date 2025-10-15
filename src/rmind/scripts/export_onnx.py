a0b670ba32dac6d6243ea6370a9fd28cd8e552a1
Evgenii Gorchakov <evgorchakov@gmail.com>
2025-10-15 12:42:30 +0200

    feat: onnx export cleanup (#179)

--- src/rmind/scripts/export_onnx.py
+++ src/rmind/scripts/export_onnx.py
@@ -1,12 +1,12 @@
 from collections.abc import Sequence
 from pathlib import Path
-from typing import Any
+from typing import Annotated, Any, ClassVar, Literal
 
 import hydra
 import torch
 from hydra.utils import instantiate
 from omegaconf import DictConfig, OmegaConf
-from pydantic.dataclasses import dataclass
+from pydantic import AfterValidator, BaseModel, ConfigDict
 from pytorch_lightning import LightningModule
 from pytorch_lightning.utilities.model_summary.model_summary import ModelSummary
 from structlog import get_logger
@@ -16,11 +16,19 @@ from rmind.config import HydraConfig
 logger = get_logger(__name__)
 
 
-@dataclass
-class Config:
+class Config(BaseModel):
+    model_config: ClassVar[ConfigDict] = ConfigDict(frozen=True, extra="ignore")
+
     model: HydraConfig[LightningModule]
-    args: Sequence[Any]
-    path: Path
+    args: Annotated[Sequence[Any], AfterValidator(instantiate)]
+    f: Path
+    opset_version: int | None = None
+    dynamo: Literal[True] = True
+    external_data: bool = False
+    optimize: bool = True
+    verify: bool = True
+    report: bool = True
+    artifacts_dir: Path = Path.cwd()
 
 
 @hydra.main(version_base=None)
@@ -31,23 +39,21 @@ def main(cfg: DictConfig) -> None:
     logger.debug("instantiating", target=config.model.target)
     args = instantiate(config.args, _recursive_=True, _convert_="all")
     model = config.model.instantiate().eval()
-    logger.debug(f"instantiated\n{ModelSummary(model)}")  # noqa: G004
+    logger.debug(f"model summary:\n{ModelSummary(model)}")  # noqa: G004
 
-    model(*args)
     logger.debug("torch exporting")
     exported_program = torch.export.export(mod=model, args=tuple(args), strict=True)
 
     logger.debug("onnx exporting")
     model = torch.onnx.export(
-        model=exported_program,
-        f=config.path,
-        dynamo=True,
-        external_data=False,
-        optimize=True,
-        verify=True,
+        model=exported_program, **config.model_dump(exclude={"model"})
     )
 
-    logger.debug("exported", path=config.path.resolve().as_posix())
+    logger.debug(
+        "exported",
+        model=config.f.resolve().as_posix(),
+        artifacts=config.artifacts_dir.resolve().as_posix(),
+    )
 
 
 if __name__ == "__main__":
