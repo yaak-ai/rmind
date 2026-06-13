04c9753351a98cc981b58b78a3c5b2e702f17f38
Harsimrat Sandhawalia <hs.sandhawalia+github@gmail.com>
2026-06-13 17:52:47 +0200

    updates to YAML

--- src/rmind/__init__.py
+++ src/rmind/__init__.py
@@ -1,5 +1,10 @@
 from importlib.metadata import version
 
+from omegaconf import OmegaConf
+
 __version__ = version(__package__ or __name__)
+
+# arithmetic in interpolations, e.g. "${eval:(${episode_length} + ${action_horizon} - 1) * 10}i"
+OmegaConf.register_new_resolver("eval", eval, replace=True)
 
 __all__ = ["__version__"]
